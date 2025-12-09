d4d4a430dc61becf5381e8a5b31c9b904bf94ae7
Badjo <bart@badjo.nl>
2025-12-09 19:50:47 +0100

    update password forget

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,6 +1,9 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
     @user = user
-    mail subject: "Reset your password", to: user.email_address
+    mail(
+      subject: "Wachtwoord resetten - Fund Together",
+      to: user.email_address
+    )
   end
 end
