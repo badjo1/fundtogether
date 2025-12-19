b291a739caf249b6aa60cbc37d3104263182726f
Badjo <bart@badjo.nl>
2025-12-20 00:30:18 +0100

    logout

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,26 @@
+// app/javascript/controllers/flash_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["message"]
+
+  connect() {
+    // Auto-dismiss after 5 seconds
+    this.timeout = setTimeout(() => {
+      this.dismiss()
+    }, 5000)
+  }
+
+  disconnect() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  dismiss() {
+    this.element.classList.add("opacity-0", "transition-opacity", "duration-300")
+    setTimeout(() => {
+      this.element.remove()
+    }, 300)
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
     # Authentication routes
   get  "login",    to: "sessions#new"
   post "login",    to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  get "logout", to: "sessions#destroy"
   
   get  "register", to: "registrations#new"
   post "register", to: "registrations#create"
